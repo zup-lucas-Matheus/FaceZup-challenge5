f30ac01a1e0e08ca684126b4fd3f11a31dea863a
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-25 16:33:01 -0300

    Metódo  para mostrar todos os zuppers cadastrados com EndPoint já declarado na classe como "zuppers".

--- src/main/java/br/com/zup/FaceZup/zupper/ZupperController.java
+++ src/main/java/br/com/zup/FaceZup/zupper/ZupperController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/zuppers")
 public class ZupperController {
@@ -12,12 +14,17 @@ public class ZupperController {
     @Autowired
     private ZupperService zupperService;
 
-
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Zupper saveZupper(@RequestBody Zupper zupper){
         return zupperService.saveZupper(zupper);
     }
 
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public List<Zupper> allZuppers(){
+        return zupperService.allZuppers();
+    }
 
 }

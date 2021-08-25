21dd74e45db91cc69a4f726f34b13c795266b074
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-25 17:08:13 -0300

    Atualizando a classe enum Cargo onde os nomes foram diminuidos.

--- src/main/java/br/com/zup/FaceZup/enums/Cargo.java
+++ src/main/java/br/com/zup/FaceZup/enums/Cargo.java
@@ -2,9 +2,9 @@ package br.com.zup.FaceZup.enums;
 
 public enum Cargo {
 
-    DESENVOLVEDOR_JR,
-    DESENVOLVEDOR_PLENO,
-    DESENVOLVEDOR_SENIO
+    DEV_JR,
+    DEV_PLENO,
+    DEV_SENIOR
 
 
 }

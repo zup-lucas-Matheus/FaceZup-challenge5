5b59f5511bcc6009e9d8a8988d4c0ce1da68d00f
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-25 16:12:04 -0300

    Fazendo a injeção do meu repositorio na minha classe service.

--- src/main/java/br/com/zup/FaceZup/zupper/ZupperService.java
+++ src/main/java/br/com/zup/FaceZup/zupper/ZupperService.java
@@ -1,10 +1,13 @@
 package br.com.zup.FaceZup.zupper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ZupperService {
 
+    @Autowired
+    private ZupperRepository zupperRepository;
 
 
 

60aef1f65b87b58a14f5ffb99a6e69db0fc33006
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-25 15:44:15 -0300

    Iniciado o projeto faceZup

---
+++ src/main/java/br/com/zup/FaceZup/FaceZupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.FaceZup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceZupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/FaceZup/FaceZupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.FaceZup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceZupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

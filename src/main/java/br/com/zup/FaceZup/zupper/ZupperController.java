2d1347218e2390b73a549f078795db97e320b43f
zup-lucas-matheus <lucas.matheus@zup.com.br>
2021-08-25 16:16:01 -0300

    Classe Controller criada.

---
+++ src/main/java/br/com/zup/FaceZup/zupper/ZupperController.java
@@ -0,0 +1,17 @@
+package br.com.zup.FaceZup.zupper;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/zuppers")
+public class ZupperController {
+
+
+
+
+
+
+
+}
